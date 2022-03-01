d22a4bdad0b2b50de818460eead06cf3ee5b177c
Justin Inho <justin@inho.dev>
2022-02-28 19:20:05 -0500

    Sha256 Hash
    Added basic hashing functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,17 +5,31 @@
 
 fn main() {
   tauri::Builder::default()
-    // this is where you pass in your custom commands. 
-    .invoke_handler(tauri::generate_handler![hash_file])
+    // this is where you pass in your custom commands.
+    .invoke_handler(tauri::generate_handler![hash_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
 
-// custom commands must have #[tauri::command] annotation.
-#[tauri::command]
-fn hash_file(file_path: String, algorithm: String) -> String {
-  println!("{}", file_path);
-  println!("{}", algorithm);
+use sha2::{Sha256};
+use std::{fs, io};
+use hex::encode_upper;
+use digest::Digest;
 
-  return String::from("test");
+#[tauri::command(async)]
+async fn hash_command(path: String, algo: String) -> String {
+  match &algo as &str {
+    "SHA256" => return hash_file(path, Sha256::default()),
+    _ => return hash_file(path, Sha256::default()),
+  }
+}
+
+fn hash_file<T>(path: String, mut hasher: T) -> String
+where
+  T: Digest,
+  T: io::Write, 
+{
+    let mut input = fs::File::open(path).unwrap();
+    io::copy(&mut input, &mut hasher).unwrap();
+    return encode_upper(hasher.finalize());
 }
