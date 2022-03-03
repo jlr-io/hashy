c6a68f6b61ac59667acbed0d61534eeb87bea0b3
Justin Inho <justin@inho.dev>
2022-03-03 14:45:53 -0500

    Added UI Packages
    Tailwind, Postcss, and daisyui

---
+++ tailwind.config.cjs
@@ -0,0 +1,39 @@
+// module.exports = {
+//   content: ['./src/**/*.{html,js,svelte,ts}'],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("daisyui")],
+//   // daisyUI config (optional)
+//   daisyui: {
+//     styled: true,
+//     themes: true,
+//     base: true,
+//     utils: true,
+//     logs: true,
+//     rtl: false,
+//     prefix: "",
+//     darkTheme: "false",
+//   },
+// }
+module.exports = {
+  mode: "jit",
+  purge: {
+    content: ["./src/**/*.{html,js,svelte,ts}"],
+    options: {
+      safelist: [/data-theme$/],
+    },
+  },
+  plugins: [require("daisyui")],
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "false",
+  },
+};
