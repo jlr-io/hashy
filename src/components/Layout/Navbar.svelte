9276c20401c658e4ddcd9f35abe28609d10d4636
Justin Inho <justin@inho.dev>
2022-04-10 18:40:22 -0400

    Navbar styling and theme

--- src/components/Layout/Navbar.svelte
+++ src/components/Layout/Navbar.svelte
@@ -1,29 +1,32 @@
 <script lang="ts">
   import Theme from "../theme/components/Theme.svelte";
-  import { link, NavigatorLocation } from "svelte-navigator";
+  import { Link, link, NavigatorLocation } from "svelte-navigator";
   import { Routes } from '../layout/models/route.models';
   import { useLocation } from "svelte-navigator";
 
   const location = useLocation();
 
   const isFirstPage = (location: NavigatorLocation) => location.pathname === Routes.selectAlgorithm;
+
+  const btnClass = 'text-xl mx-2 btn btn-sm btn-base-200'
+
+  const btnDisabledClass = 'text-xl mx-2 btn btn-sm btn-disabled'
 </script>
 
-<!-- navbar -->
+<!-- bg-neutral text-neutral-content -->
+
 <header class="select-none">
-  <div class="navbar bg-neutral text-neutral-content mb-0">
+  <div class="navbar bg-base-300 mb-0">
     <div class="flex-none">
       <!-- ❮ -->
       <!-- ❯ -->
-      <button class="btn text-xl" disabled={isFirstPage($location)}>
-        <a use:link href="{Routes.selectAlgorithm}">❮</a>
-      </button>
-      <button class="btn text-xl" disabled={!isFirstPage($location)}>
-        <a use:link href="{Routes.computeHash}">❯</a>
-      </button>
+
+      <a use:link href="{Routes.selectAlgorithm}" role="button" class="{!isFirstPage($location) ? btnClass : btnDisabledClass}">❮</a>
+      <a use:link href="{Routes.computeHash}" role="button" class="{isFirstPage($location) ? btnClass : btnDisabledClass}">❯</a>
+
     </div>
     <div class="flex-1">
-      HASHY!!
+      <!-- app title goes here. -->
     </div>
 
     <div class="flex-none">
