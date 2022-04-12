9565be6558bcf0d7731f2395fa57ace760ac6acc
Justin Inho <justin@inho.dev>
2022-04-12 16:41:33 -0400

    Svelte Awesome
    Added svelte awesome icons and loading.

--- src/pages/ComputeHash.svelte
+++ src/pages/ComputeHash.svelte
@@ -1,4 +1,19 @@
 <script lang="ts">
+  import Icon from "svelte-awesome";
+  import {
+    beer,
+    clipboard,
+    archive,
+    close,
+    code,
+    spinner,
+    terminal,
+    comment,
+    codeFork,
+    camera,
+    ban,
+  } from "svelte-awesome/icons";
+
   import { dialog, invoke } from "@tauri-apps/api";
   import { dialogOptions } from "../components/file/models/file.model";
   import {
@@ -13,7 +28,7 @@
   } from "../components/file/store/file.store";
   import { store as hashStore } from "../components/hash/store/hash.store";
   import { Hash, LOADING } from "../components/hash/models/hash.models";
-  
+
   const onBrowse = async () => {
     fileStore.set((await dialog.open(dialogOptions)) as string);
     hashStore.reset();
@@ -50,134 +65,162 @@
     return !hasFilePath;
   }
 
-  function isLoading(value: string) {
+  function isLoading(value: string): boolean {
     return value === LOADING;
   }
+
+  let isAnyLoading;
+  $: $hashStore, (isAnyLoading = [...$hashStore.values()].includes(LOADING));
 </script>
 
 <!-- info row -->
-<div class="flex flex-wrap mb-4">
+<div class="flex flex-wrap mb-6 md:mb-4">
   <!-- file -->
-  <div class="flex justify-center items-center h-20 w-full md:w-1/2 md:pt-5 md:mb-0 md:h-32">
-    {#if !$hasFilePath}
-      <p class="text-xl py-2 text-center">
-        Press the "Browse" button to select a file.
-      </p>
-    {:else}
-      {#if $fileMetaData}
-        {#await $fileMetaData then metadata}
-
-          <!-- small screens show only file name  -->
-          <div class="flex text-center md:hidden">
-            <p class="text-xl truncate w-64">{metadata.name}</p>
-          </div>
-
-          <!-- metadata stat | hide on small screens-->
-          <div class="hidden md:flex">
-
-            <!-- indicator -->
-            <div class="indicator">
-              <div class="indicator-item indicator-top indicator-end">
-                <button class="btn btn-xs" on:click={onClear}>X</button>
-              </div>
-            
-            <!-- end indicator -->
+  <div
+    class="flex justify-center items-center h-20 w-full mt-1 md:w-1/2 md:pt-5 md:mb-0 md:h-32"
+  >
+    {#if $fileMetaData}
+      {#await $fileMetaData then metadata}
+        <div class="flex">
+          <!-- indicator -->
+          <div class="indicator">
+            <div class="indicator-item indicator-top indicator-end">
+              <button
+                class="btn btn-xs"
+                disabled={disableClearButton($hasFilePath) || isAnyLoading}
+                on:click={onClear}
+              >
+                <Icon
+                  data={close}
+                  scale={1.25}
+                  style="color: hsl(var(--er));"
+                /></button
+              >
+            </div>
 
             <!-- stat -->
-            <div class="stats shadow bg-base-300 max-w-72">
+            <div class="stats shadow-md bg-base-300 max-w-72">
               <div class="stat">
-                <div class="stat-title">{metadata.size}</div>
+                <div class="stat-title hidden md:inline-block">
+                  {metadata.size}
+                </div>
                 <div class="stat-value text-lg truncate">{metadata.name}</div>
-                <div class="stat-desc">
+                <div class="stat-desc hidden md:inline-block">
                   <div>Last Modified |</div>
                   {metadata.last_modified}
                 </div>
               </div>
             </div>
           </div>
-          <!-- end stat -->
-
-          </div>
-          <!-- end indicator -->
-
-        {:catch}
-          <p class="hidden">No file was chosen yet.</p>
-        {/await}
-      {/if}
+        </div>
+      {:catch}
+        <p class="hidden">No file was chosen yet.</p>
+      {/await}
     {/if}
   </div>
 
-  <!-- buttons -->
-  <div class="flex flex-wrap justify-center items-center w-full md:w-1/2 h-8 md:pt-5 md:h-32">
-    <button
-      class="btn btn-sm md:btn mx-1 md:mx-3"
-      disabled={disableComputeButton($hasFilePath, $hasAlgorithms)}
-      on:click={onCompute}>Compute
-    </button>
+  <div
+    class="flex flex-wrap justify-center items-center h-8 w-full md:w-1/2 md:pt-5 md:h-32"
+  >
+    {#if $hasFilePath}
+      <div class="hidden w-3/4 indicator text-center md:inline-block">
+        <!-- <div class="indicator"> -->
+        <span class="indicator-item indicator-middle badge"
+          ><button><Icon data={clipboard} /></button></span
+        >
+        <input
+          type="text"
+          placeholder="Paste hash to verify"
+          class="input input-bordered w-full"
+        />
+        <!-- </div> -->
+      </div>
+    {:else}
+      <p class="text-lg w-full text-center">
+        Press the "Browse" button to select a file.
+      </p>
+    {/if}
 
-    <button 
-      class="btn btn-sm md:btn mx-1 md:mx-3" 
-      on:click={onBrowse}>Browse
-    </button>
+    <!-- buttons -->
+    <div class="w-full text-center">
+      <div class="tooltip" data-tip="Hash">
+        <button
+          class="hashy-button btn-circle"
+          class:loading={isAnyLoading}
+          disabled={disableComputeButton($hasFilePath, $hasAlgorithms) ||
+            isAnyLoading}
+          on:click={onCompute}
+        >
+          {#if !isAnyLoading}
+            <Icon data={code} scale={1.25} />
+          {/if}
+        </button>
+      </div>
 
-    <button
-      class="btn btn-sm md:btn mx-1 md:mx-3"
-      disabled={disableClearButton($hasFilePath)}
-      on:click={onClear}>Clear
-    </button>
+      <div class="tooltip" data-tip="Browse">
+        <button
+          class="hashy-button btn-circle"
+          disabled={isAnyLoading}
+          on:click={onBrowse}
+        >
+          <Icon data={archive} scale={1.25} />
+        </button>
+      </div>
+    </div>
   </div>
-  <!-- end buttons -->
-
 </div>
-<!-- end info row -->
-
 
 <!-- hash row -->
 <div class="flex mb-4">
-
   <!-- container -->
   <div class="container mx-auto h-[380px] lg:h-[500px] overflow-y-auto">
-
     <!-- hashes -->
     {#if $algorithmStore}
-        {#each $algorithmStore as algo}
+      {#each $algorithmStore as algo}
+        <!-- hash card -->
+        <div
+          class="h-1/3 lg:h-1/4 border-2 rounded-lg border-base-100 bg-base-300"
+        >
+          <!-- header -->
+          <p class="text-base px-1 py-1 md:text-xl md:px-2 md:py-2">
+            <b>{algo}</b>
+          </p>
+          <!-- end header -->
 
-          <!-- hash card -->
-          <div class="h-1/3 lg:h-1/4 border rounded-lg border-base-100 bg-base-300">
-
-            <!-- header -->
-            <p class="text-base px-1 py-1 md:text-xl md:px-2 md:py-2">
-              <b>{algo}</b>
+          <!-- body -->
+          {#if $hashStore.get(algo) && !isLoading($hashStore.get(algo))}
+            <p class="break-all text-sm px-2 md:text-base md:px-4">
+              {$hashStore.get(algo)}
             </p>
-            <!-- end header -->
-            
-            <!-- body -->
-            {#if $hashStore.get(algo) && !isLoading($hashStore.get(algo))}
-              <div class="break-all text-sm px-2 md:text-base md:px-4">
-                {$hashStore.get(algo)}
-              </div>
-            {:else if isLoading($hashStore.get(algo))}
-              <div class="flex justify-center">
-                <progress />
-              </div>
-            {/if}
-            <!-- end body -->
-
-          </div>
-          <!-- end hash card -->
-        {/each}
-
+          {:else if isLoading($hashStore.get(algo))}
+            <div class="flex justify-center">
+              <!-- <progress /> -->
+              <!-- <Shadow size="30" color="primary"/> -->
+              <Icon
+                data={spinner}
+                pulse
+                scale={3}
+                style="color: hsl(var(--p));"
+              />
+            </div>
+          {/if}
+          <!-- end body -->
+        </div>
+        <!-- end hash card -->
+      {/each}
     {/if}
     <!-- end hashes -->
-
   </div>
   <!-- end container  -->
-
 </div>
-<!-- end row -->
 
+<!-- end row -->
 <style>
   progress {
     border: none;
+  }
+
+  .max-w-72 {
+    max-width: 18rem;
   }
 </style>
